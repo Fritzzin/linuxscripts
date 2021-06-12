ef2b07d78ee90c040f2c02709779a49edd476bc8
Augusto Fritz <augusto.fritz@universo.univates.br>
2021-06-11 23:59:57 -0300

    Added variables

--- Installation.sh
+++ Installation.sh
@@ -1,103 +1,87 @@
 #!/bin/sh
 
-echo "UPDATE AND INSTALL UPDATES"
+# Variables
+BRAVE_KEYRING_PATH="/usr/share/keyrings/brave-browser-archive-keyring.gpg"
+BRAVE_KEYRING="https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"
+
+BRAVE_REPOSITORY="https://brave-browser-apt-release.s3.brave.com/"
+BRAVE_REPOSITORY_PATH="/etc/apt/sources.list.d/brave-browser-release.list"
+
+SCRIPTS_FOLDER="$HOME/.scripts"
+
+
+echo -e "\n\n UPDATE AND INSTALL UPDATES"
 sudo apt update && sudo apt upgrade -y
-echo ""
-echo ""
 
-echo "BRAVE"
+
+echo -e "\n\n CURL AND WGET"
+sudo apt install wget curl -y
+
+
+echo -e "\n\n BRAVE"
 sudo apt install apt-transport-https curl -y
-
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
+sudo curl -fsSLo $BRAVE_KEYRING_PATH $BRAVE_KEYRING
+echo "deb [signed-by=$BRAVE_KEYRING_PATH arch=amd64] $BRAVE_REPOSITORY stable main"|sudo tee $BRAVE_REPOSITORY_PATH
 sudo apt update -y
-
 sudo apt install brave-browser -y
 
 
-echo "DEVELOPMENT"
-sudo apt install build-essential wget curl openssh-client default-jdk default-jre vim nano git nodejs npm -y
-echo ""
-echo ""
+echo -e "\n\n DEVELOPMENT"
+sudo apt install build-essential openssh-client default-jdk default-jre vim git nodejs npm -y
 
-echo "BATTERY MANAGEMENT"
+
+echo -e "\n\n BATTERY MANAGEMENT"
 sudo apt install tlp tlp-rdw -y
-echo ""
-echo ""
 
-echo "SECURITY"
+
+echo -e "\n\n SECURITY"
 sudo apt install ufw -y
-echo ""
-echo ""
 
-echo "BACKUP TOOLS"
+
+echo -e "\n\n BACKUP TOOLS"
 sudo apt install timeshift -y
-echo ""
-echo ""
 
 
-echo "MULTIMEDIA CODECS"
+echo -e "\n\n MULTIMEDIA CODECS"
 sudo apt install vlc -y
-echo ""
-echo ""
 
 
-echo "COMPRESSION"
+echo -e "\n\n COMPRESSION"
 sudo apt install unzip gzip p7zip-full p7zip-rar unrar unrar-free -y
-echo ""
-echo ""
 
 
-echo "MISC/THEMING"
+echo -e "\n\n MISC/THEMING"
 sudo apt install neofetch papirus-icon-theme software-properties-common -y
-echo ""
-echo ""
 
 
-echo "NANO SYNTAX HIGHLIGHT"
-wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
-echo ""
-echo ""
-
-
-echo "ENABLE FIREWALL"
+echo -e "\n\n ENABLE FIREWALL"
 sudo ufw enable
 sudo systemctl enable ufw
-echo ""
-echo ""
 
 
-echo "ENABLE TLP"
+echo -e "\n\n ENABLE TLP"
 sudo tlp start
 sudo systemctl enable tlp
-echo ""
-echo ""
 
 
-echo "CREATING BASH ALIASES"
+echo -e "\n\n CREATING BASH ALIASES"
 touch ~/.bash_aliases
 echo "
 # BASH
-alias bashrc=\"nano ~/.bashrc\"
-alias aliases=\"nano ~/.bash_aliases\"
+alias bashrc=\"nano ${HOME}/.bashrc\"
+alias aliases=\"nano ${HOME}/.bash_aliases\"
 
 # MAINTENANCE
-alias update=\"~/.scripts/Update.sh\"
-alias clean=\"~/.scripts/Clean.sh\"
-
+alias update=\"${SCRIPTS_FOLDER}/Update.sh\"
+alias clean=\"${SCRIPTS_FOLDER}/Clean.sh\"
 " >> ~/.bash_aliases
-echo ""
-echo ""
 
 
-echo "CREATING SCRIPTS FOLDER AND MOVING SCRIPTS"
-mkdir ~/.scripts
-mv Update.sh ~/.scripts
-mv Clean.sh ~/.scripts
-mv MonoAudio.sh ~/.scripts
-echo ""
-echo ""
+echo -e "\n\n CREATING SCRIPTS FOLDER AND MOVING SCRIPTS"
+mkdir $SCRIPTS_FOLDER
+mv Update.sh $SCRIPTS_FOLDER
+mv Clean.sh $SCRIPTS_FOLDER
+mv MonoAudio.sh $SCRIPTS_FOLDER
 
-echo "DONE"
+
+echo -e "\n\n DONE \n\n"
